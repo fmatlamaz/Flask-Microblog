ab0698af450cda88545fa15d7f68b08875457405
Fatih Atlamaz <atlamaz2@illinois.edu>
2024-01-11 08:10:05 -0500

    Halfway into Chapter 5

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,22 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from urllib.parse import urlsplit
 
 #The two strange @app.route lines above the function are decorators a unique feature of the Python language. 
 #A decorator modifies the function that follows it. 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username': 'Fatih'}
     posts = [
         {
             'author': {'username': 'Yavuz'},
@@ -21,13 +29,30 @@ def index():
         }
     ]
     #The operation that converts a template into a complete HTML page is called rendering
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        # If the login URL does not have a next argument, then the user is redirected to the index page.
+        # If the login URL includes a next argument that is set to a relative path (or in other words, a URL without the domain portion), then the user is redirected to that URL.
+        # If the login URL includes a next argument that is set to a full URL that includes a domain name, then this URL is ignored, and the user is redirected to the index page.
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
