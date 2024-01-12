48d85f23445934fb6eb7f5f80bf557f8230e353c
Fatih <fmatlamaz@outlook.com>
2024-01-11 19:45:37 -0500

    Finished Chapter 9: Pagination

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,8 @@ from app.forms import RegistrationForm
 from datetime import datetime, timezone
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
+from app.forms import PostForm
+from app.models import Post
 
 @app.before_request
 def before_request():
@@ -22,23 +24,30 @@ def before_request():
 
 #The two strange @app.route lines above the function are decorators a unique feature of the Python language. 
 #A decorator modifies the function that follows it. 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'Yavuz'},
-            'body': 'Im too busy working on Speech and Debate!'
-        },
-        {
-            'author': {'username': 'Beyza'},
-            'body': 'iPad istiyorum!'
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
 
-        }
-    ]
     #The operation that converts a template into a complete HTML page is called rendering
-    return render_template('index.html', title='Home', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = db.paginate(current_user.following_posts(), page=page,per_page=app.config['POSTS_PER_PAGE'], error_out=False)    
+    next_url = url_for('index', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) \
+        if posts.has_prev else None
+
+
+    return render_template('index.html', title='Home', form=form,posts=posts.items, next_url=next_url, prev_url=prev_url)
+
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -84,12 +93,16 @@ def register():
 @login_required
 def user(username):
     user = db.first_or_404(sa.select(User).where(User.username == username))
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
-    ]
+    page = request.args.get('page', 1, type=int)
+    query = user.posts.select().order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page,per_page=app.config['POSTS_PER_PAGE'],error_out=False)
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items,next_url=next_url, prev_url=prev_url, form=form)
+
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -147,4 +160,16 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
 
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.paginate(query, page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) \
+        if posts.has_prev else None
+    
+    return render_template("index.html", title='Explore', posts=posts.items,next_url=next_url, prev_url=prev_url)
 
--- config.py
+++ config.py
@@ -11,4 +11,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['fmatlamaz@gmail.com']
+    POSTS_PER_PAGE = 3
+
 
